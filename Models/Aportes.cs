526e81c30f62fd2a85ea19490cda9bbe891c17ea
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-02-17 19:45:46 -0400

    Se modifico el modelo aportes

--- Models/Aportes.cs
+++ Models/Aportes.cs
@@ -1,29 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Models
 {
     public class Aportes
     {
         [Key]
-        public int CobroId { get; set; }
+        public int AporteId { get; set; }
 
         [Required(ErrorMessage = "Campo obligatorio")]
         public DateTime Fecha { get; set; } = DateTime.Now;
 
         [Required(ErrorMessage = "Campo obligatorio")]
-        [Range(1, double.MaxValue)]
-        public double Monto { get; set; }
+        [StringLength(100, ErrorMessage = "El nombre no puede superar los 100 caracteres")]
+        public string Persona { get; set; } = string.Empty;
+
+        [StringLength(200, ErrorMessage = "La observación no puede superar los 200 caracteres")]
+        public string Observacion { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Campo obligatorio")]
-        [Range(1, double.MaxValue, ErrorMessage = "Debe selecionar un deudor valido")]
-        public int DeudorId { get; set; }
-
-        [InverseProperty("Cobro")]
-        public virtual ICollection<AportesDetalles> CobrosDetalle { get; set; } = new List<CobrosDetalles>();
-
-        [ForeignKey("DeudorId")]
-        [InverseProperty("Cobros")]
-        public virtual Deudores Deudor { get; set; } = null!;
-
+        [Range(0.01, double.MaxValue, ErrorMessage = "El monto debe ser mayor a 0")]
+        public decimal Monto { get; set; }
     }
 }
