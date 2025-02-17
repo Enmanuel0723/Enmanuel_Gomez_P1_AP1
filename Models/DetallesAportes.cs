bef35cf5ba0d509443af2bb306a25e3ade5e3ce4
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-02-17 18:44:54 -0400

    Se creo el modelo #detalles Aportes

---
+++ Models/DetallesAportes.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+public class AportesDetalles
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    public int CobroId { get; set; }
+
+    public int PrestamoId { get; set; }
+
+    public double ValorCobrado { get; set; }
+
+    [ForeignKey("CobroId")]
+    [InverseProperty("CobrosDetalle")]
+    public virtual aportes Aportes { get; set; } = null!;
+
+
+
+}
